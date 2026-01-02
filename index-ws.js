cc4aafcebf86df00acd12a3576d15ec40d7fc59c
Basil <basil@kali>
2026-01-02 15:32:26 +0530

    DB added

--- index-ws.js
+++ index-ws.js
@@ -9,9 +9,26 @@ app.get('/',function(req,res){
 
 });
 
+
+
 server.on('request',app);
 
-server.listen(5000,function(){console.log("Server Started on Port 5000")});
+server.listen(3000,function(){console.log("Server Started on Port 3000")});
+
+process.on("SIGINT",()=>{
+
+
+	wss.clients.forEach(function each(client){
+		client.close();
+
+	})
+
+	server.close(()=>{
+		console.log("Shutting down db");
+		shutdownDB();
+
+	})
+})
 
 /**Begin Websocket**/
 
@@ -29,6 +46,12 @@ wss.on('connection',function connection(ws){
 		ws.send("Welcome to my server");
 	}
 
+	db.run(
+
+		`INSERT INTO visitors (count,time) VALUES (${numClients}, datetime('now'))`
+
+
+	)
 	ws.on('close',function close(){
 
 		console.log("Client disconnected");
@@ -40,4 +63,41 @@ wss.broadcast = function broadcast(data){
 		client.send(data);
 
 	});
+}
+
+/**End Websocket**/
+
+/** Begin Database **/
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(':memory:');
+
+db.serialize(()=>{
+
+	db.run(`
+
+	CREATE TABLE visitors (
+	        count INTEGER,
+		time TEXT
+	)
+
+	`)
+
+
+});
+
+function getCounts(){
+
+	db.each("SELECT * FROM visitors",(error,row)=>{
+
+		console.log(row);
+
+	})
+
+}
+
+function shutdownDB(){
+	getCounts();
+	console.log("Shutting down db")
+	db.close();
 }
